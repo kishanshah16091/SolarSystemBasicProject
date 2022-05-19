b9ddcd8de77892a0a4d5327cc251302b72eaab08
kishanshah16091 <kishan.shah16091@gmail.com>
2022-05-18 20:00:09 -0400

    Planet1 features have been added successfully

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,13 @@ public class Main {
         FacX.color="green";
         FacX.radius="738y4932749km";
         FacX.size="9999999999m";
+
+        //adding features for planet1
+
+        Planet1Features GulK= new Planet1Features();
+        GulK.color="blue";
+        GulK.size="3848325828km";
+        GulK.name="preacheX";
+
     }
 }
